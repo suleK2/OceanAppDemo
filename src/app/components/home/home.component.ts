b8646b1226558212366efb1122cca7e6fcae69fb
Sule <sulekalkankaya@gmail.com>
2025-05-22 05:37:08 +0300

    some styling part 2

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-
+import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'app-home',
+  imports : [TranslateModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
