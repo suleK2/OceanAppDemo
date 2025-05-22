56427d603ce2c9958e2a00424eafa1a78514fc23
Sule <sulekalkankaya@gmail.com>
2025-05-22 04:53:33 +0300

    converting crew card to a modal

--- src/app/components/crew-card/crew-card.component.ts
+++ src/app/components/crew-card/crew-card.component.ts
@@ -1,18 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CrewMember } from '../../models/crew-member.model';
 import { TranslateModule } from '@ngx-translate/core';
-import { CommonModule } from '@angular/common';
-
 import { CrewMemberComponent } from '../crew-member/crew-member.component';
 import { CrewCertificatesComponent } from '../crew-certificates/crew-certificates.component';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-crew-card',
-  imports: [TranslateModule,CommonModule,CrewMemberComponent,CrewCertificatesComponent],
   templateUrl: './crew-card.component.html',
+  imports: [TranslateModule,CrewMemberComponent,CrewCertificatesComponent,CommonModule]
 })
-
 export class CrewCardComponent {
-  @Input() member!: CrewMember;
+  @Input() member?: CrewMember;
+  @Output() close = new EventEmitter<void>();
+
   activeTab: 'member' | 'certificates' = 'member';
+
+  closeModal() {
+    this.close.emit();
+  }
 }
